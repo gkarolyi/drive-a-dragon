0ffd15aff40978f0e4363b0ee555eb87c5c6b2f8
Matthias Kutz <matthias.kutz@gmx.net>
2021-02-23 15:28:22 +0100

    seeds.rb fixed

---
+++ app/policies/vehicle_policy.rb
@@ -0,0 +1,15 @@
+class VehiclePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def show?
+    true
+  end
+end
