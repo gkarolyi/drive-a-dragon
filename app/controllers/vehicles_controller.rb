fc276d1351610565bebda1386f6dc4b1c9a4c873
Matthias Kutz <matthias.kutz@gmx.net>
2021-02-24 11:45:38 +0100

    User Image upload (cloudinary) added

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -24,6 +24,6 @@ class VehiclesController < ApplicationController
   private
 
   def vehicle_params
-    params.require(:vehicle).permit(:name, :description, :location, :price)
+    params.require(:vehicle).permit(:name, :description, :location, :price, photos: [])
   end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,11 +2,12 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  has_many_attached :photos
 
   validates :name, uniqueness: true
   validates :user, presence: true
   validates :price, presence: true
-  validates :category, inclusion: { in: :category? }
+  # validates :category, inclusion: { in: :category? }
 
   private
 
