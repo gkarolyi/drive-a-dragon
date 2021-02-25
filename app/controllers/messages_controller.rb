52d6be555b0f84c6bbca2f1f6085c04f31f992f6
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-02-25 11:21:19 +0100

    saving

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,18 @@
 class MessagesController < ApplicationController
   def index
-    @messages = policy_scope(Message)
+    @messages = policy_scope(Message).where(sender_id: current_user.id).or(Message.where(receiver_id: current_user.id))
+
     authorize @messages
   end
 
-  def show 
-        @sender = User.find(params[:users][:ids])
-        @receiver = User.find(params[:receiver_id])
-        @messages = Message.where(sender_id: @sender.id, receiver_id: @receiver.id).or(Message.where(sender_id: @receiver.id, receiver_id: @sender.id))
-        @chat_id = [@sender.id, @receiver.id].sort.join("") #generates a unique identifier for a pair of user 
-    end 
+  def show
+    raise
+    @sender = User.find(params[:users][:ids])
+    @receiver = User.find(params[:receiver_id])
+    @messages = Message.where(sender_id: @sender.id, receiver_id: @receiver.id).or(Message.where(sender_id: @receiver.id, receiver_id: @sender.id))
+    @chat_id = [@sender.id, @receiver.id].sort.join("") #generates a unique identifier for a pair of user 
+  end
+
+  def new
+  end
 end
