8bb9887d2f238136e021a36a1d5b85f111521bfa
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-02-26 14:56:55 +0100

    added active tab marker to navbar

--- app/javascript/plugins/active_navbar.js
+++ app/javascript/plugins/active_navbar.js
@@ -1,5 +1,18 @@
-const navContainer = document.getElementById("navbarSupportedContent");
+const initNavbarActive = () => {
+  const navContainer = document.getElementById("navbarSupportedContent");
 
-if (navContainer) {
-  const navPanes = document.getElementsByClassName("nav-item");
-}
+  if (navContainer) {
+    const navPanes = Array.from(document.getElementsByClassName("nav-item"));
+
+    navPanes.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        event.preventDefault();
+        const current = document.getElementsByClassName("active")[0];
+        current.classList.remove("active");
+        event.target.classList.add("active");
+      });
+    });
+  }
+};
+
+export { initNavbarActive };
